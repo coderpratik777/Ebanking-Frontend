016450f871023027ead09cdea9598c36e6909aa2
Amitthelkar <thelkarac@gmail.com>
2023-03-08 22:11:17 +0530

    front end push

---
+++ ebankingapp/src/app/Components/accountstatement/accountstatement.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accountstatement',
+  templateUrl: './accountstatement.component.html',
+  styleUrls: ['./accountstatement.component.css']
+})
+export class AccountstatementComponent {
+
+  accountnumber!:Number;
+  name!:String;
+  accounttype!:String;
+  balance!:Number;
+
+
+  show(){
+
+    
+  }
+
+
+
+}
--- ebankingapp/src/app/Components/login/login.component.ts
+++ ebankingapp/src/app/Components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  username!:String;
+  password!:String;
+
+  logindetail!:any;
+
+  constructor (private http:HttpClient){
+
+  }
+
+  login(logs:{email:string,password:string}){
+    
+
+    let json:any=JSON.stringify(logs);
+    const headers= new HttpHeaders({'content-type':'application/json'})
+    
+    this.http.post(
+      'http://localhost:8080/login',
+      json,{headers:headers}).subscribe((Response)=>{
+        this.logindetail=Response;
+
+      })
+  
+  }
 
 }
