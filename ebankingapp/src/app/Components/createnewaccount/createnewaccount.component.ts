ef6867761d7fd0677c7b9825142c852d9766af37
coderpratik777 <coderpratik321@gmail.com>
2023-03-09 22:12:21 +0530

    create new account module added

--- ebankingapp/src/app/Components/createnewaccount/createnewaccount.component.ts
+++ ebankingapp/src/app/Components/createnewaccount/createnewaccount.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./createnewaccount.component.css']
 })
 export class CreatenewaccountComponent {
+
+  customerData:CustomerData=new CustomerData();
+  createAccountStatus!:any;
+
+  constructor(private http:HttpClient){
+
+  }
+  submit(){
+    let url=`http://localhost:8080/createaccount`;
+    let data=JSON.stringify(this.customerData);
+    this.http.post(url,data,{
+      headers:{
+        "Content":"appliaction/json,charset=utf-8"
+  
+      }
+    }).subscribe((response)=>{
+      console.log(response);
+      this.createAccountStatus=response;
+
+    })
+  }
+
+
+}
+
+export class CustomerData{
+  firstName!:String;
+  lastName!:String;
+  fatherName!:String;
+  email!:string;
+  datOfBirth!:Date;
+  mobileNumber!:String;
+  sourceOfIncome!:String;
+  grossAnnualIncome!:number;
+  addharCardNumber!:number;
+  address:Address=new Address();
+}
+export class Address{
+  addressLine1!:string;
+  addressLine2!:string;
+  landmark!:string;
+  pincode!:number;
+  city!:number;
 
 }
